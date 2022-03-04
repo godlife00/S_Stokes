1bb543966296006e52ff0690ddf6a23dad26872b
godlife00 <godlife00@gmail.com>
2022-03-04 13:09:28 +0900

    메인페이지 1차 완성

--- js/S_Stokes.js
+++ js/S_Stokes.js
@@ -6,7 +6,31 @@ $(document).ready(function () {
         slidesPerView: "auto",
         spaceBetween: 8,
     });
+    var swiper = new Swiper('.maingameSwiper', {
+        slidesPerView: "auto",
+        spaceBetween: 8,
+    });
+
+
+    // 상단 검색바
+    $('.my_info').on('click', function () {
+        $('.mypage_info').modal({
+            fadeDuration: 100
+        });
+    });
     
+    // 상단 검색바
+    $('.autocompleteWrap, .bg_layer').hide();
+    // $('.bgWrap .r_menu a.searchForm').on("focusout", function () {            
+    //     $('.AutoComplete, .bg_layer').hide();
+    // });
+    $('.bg_layer').on("click", function () {            
+        $('.AutoComplete, .bg_layer').hide();
+    });
+    $('.bgWrap .r_menu a.searchForm').on("click", function () {            
+        $('.AutoComplete').slideToggle(300);
+        $('.bg_layer').show();
+    });
         
 });
 
