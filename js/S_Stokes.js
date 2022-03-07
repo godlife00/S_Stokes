cef5a64e1e4cc008895030655bcb74085589e4cb
godlife00 <godlife00@gmail.com>
2022-03-07 16:58:23 +0900

    검색결과

--- js/S_Stokes.js
+++ js/S_Stokes.js
@@ -31,6 +31,50 @@ $(document).ready(function () {
         $('.AutoComplete').slideDown(300);
         $('.bg_layer').show();
     });
+
+    // 오늘의승부주 탭네비 상단 고정 시키기    
+    $(window).scroll(function () {        
+        if ($('.mainBox.game_scr').length) {
+            var jbOffset = $('.mainBox.game_scr').offset();                                            
+            if ($(document).scrollTop() < jbOffset.top - 34) {      
+                $('.mainBox.game_scr').css({
+                    'padding' : '25px 0 25px',                    
+                });                                                      
+                $('.mainBox.game_scr .page_nav').css({
+                    'position' : 'relative',
+                    'top': '0',
+                }).removeClass('boxShadow');     
+                
+            } else {                            
+                $('.mainBox.game_scr').css({
+                    'padding' : '62px 0 25px',                    
+                });                
+                $('.mainBox.game_scr .page_nav').css({
+                    'position' : 'fixed',
+                    'top': '58px',                    
+                }).addClass('boxShadow');              
+            }
+        }        
+    });
+
+    // 오늘의승부주 탭네비 페이지내 이동
+    $('.mainBox.game_scr .page_nav li a.nav_game_scr').on("click", function () {
+        var offset = $('html').offset();
+        $('html').animate({ scrollTop: offset.top}, 400);
+    });
+    $('.mainBox.game_scr .page_nav li a.nav_portfolio').on("click", function () {
+        var offset = $('.mainBox.portfolio').offset();
+        $('html').animate({ scrollTop: offset.top - 80 }, 400);
+    });
+    $('.mainBox.game_scr .page_nav li a.nav_strategy').on("click", function () {
+        var offset = $('.mainBox.strategy').offset();
+        $('html').animate({ scrollTop: offset.top - 80 }, 400);
+    });
+    $('.mainBox.game_scr .page_nav li a.nav_attr').on("click", function () {
+        var offset = $('.mainBox.attr').offset();
+        $('html').animate({ scrollTop: offset.top - 80 }, 400);
+    });
+
         
 });
 
